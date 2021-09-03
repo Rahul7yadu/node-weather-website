de9f5be7880da513bee7a0581993ea5ca1bcc408
Rahul yadu <rahulyadu17@gmail.com>
2021-09-03 20:29:26 +0530

    added more information

--- public/js/app.js
+++ public/js/app.js
@@ -4,14 +4,16 @@ const form = document.querySelector("form");
 const input = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
 const messageTwo = document.querySelector("#message-2");
-
+const main = document.querySelector(".main")
+const img = document.querySelector(".img")
+const humidity = document.querySelector(".humidity")
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (input.value == "") {
     return;
   }
   messageOne.textContent = "loading...";
-
+   
   fetch(`/weather?address=${input.value}`).then(
     (response) => {
       response.json().then((data) => {
@@ -20,8 +22,13 @@ form.addEventListener("submit", (e) => {
           messageOne.textContent = data.error;
           return;
         }
-        messageOne.textContent = "it is " + data.forecast.temperature + "°C";
+        messageOne.textContent = "it is " + data.forecast.temperature + "°C. but  feels like " + data.forecast.feelslike + " °C.";
         messageTwo.textContent = "in " + data.location;
+        main.textContent = data.forecast.weather_descriptions[0];
+        img.src = data.forecast.weather_icons[0]
+        humidity.textContent = "Humidity:" + data.forecast.humidity
+        console.log("hello ji")
+        
       });
     }
   );
